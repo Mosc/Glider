b4594caf8aaee5c006b194a6f23a4840c19cbf80
Mosc <daniel.moscoviter@gmail.com>
2020-09-27 10:43:07 +0200

    Add copy text action

--- lib/widgets/items/item_tile_data.dart
+++ lib/widgets/items/item_tile_data.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:glider/models/item.dart';
 import 'package:glider/models/item_type.dart';
@@ -79,6 +80,20 @@ class ItemTileData extends HookWidget {
                           },
                         ),
                       ],
+                      if (item.text != null)
+                        ListTile(
+                          title: const Text('Copy text'),
+                          onTap: () async {
+                            await Clipboard.setData(
+                                ClipboardData(text: item.text));
+                            Scaffold.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('Text has been copied'),
+                              ),
+                            );
+                            Navigator.of(context).pop();
+                          },
+                        ),
                       ListTile(
                         title: const Text('Share comment tree'),
                         onTap: () async {
