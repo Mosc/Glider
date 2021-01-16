1bd16b45cc63ec335882a97be99a888a4d5aff34
Mosc <daniel.moscoviter@gmail.com>
2021-01-16 23:48:51 +0100

    Move non-search stories actions to overflow menu

--- lib/pages/stories_page.dart
+++ lib/pages/stories_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:glider/models/menu_action.dart';
 import 'package:glider/models/story_type.dart';
 import 'package:glider/pages/account_page.dart';
 import 'package:glider/pages/favorites_page.dart';
@@ -14,8 +15,6 @@ import 'package:glider/providers/repository_provider.dart';
 import 'package:glider/utils/app_bar_util.dart';
 import 'package:glider/utils/scaffold_messenger_state_extension.dart';
 import 'package:glider/utils/uni_links_handler.dart';
-import 'package:glider/widgets/common/smooth_animated_cross_fade.dart';
-import 'package:glider/widgets/common/smooth_animated_switcher.dart';
 import 'package:glider/widgets/items/stories_body.dart';
 import 'package:hooks_riverpod/all.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -51,9 +50,6 @@ class StoriesPage extends HookWidget {
     final StateController<StoryType> storyTypeStateController =
         useProvider(storyTypeStateProvider);
 
-    final AsyncValue<Iterable<int>> favoritesValue =
-        useProvider(favoriteIdsProvider);
-
     final AsyncValue<bool> loggedInValue = useProvider(loggedInProvider);
 
     return Scaffold(
@@ -65,23 +61,37 @@ class StoriesPage extends HookWidget {
             leading: AppBarUtil.buildFluentIconsLeading(context),
             title: const Text('Glider'),
             actions: <Widget>[
-              _buildSearchIcon(context),
-              _buildSubmitIcon(context),
-              favoritesValue.maybeWhen(
-                data: (Iterable<int> favorites) => SmoothAnimatedSwitcher(
-                  axis: Axis.horizontal,
-                  condition: favorites.isNotEmpty,
-                  child: _buildFavoritesIcon(context),
-                ),
-                orElse: () => const SizedBox.shrink(),
+              IconButton(
+                icon: const Icon(FluentIcons.search_24_filled),
+                tooltip: 'Search',
+                onPressed: () => _searchSelected(context),
               ),
-              loggedInValue.maybeWhen(
-                data: (bool loggedIn) => SmoothAnimatedCrossFade(
-                  condition: loggedIn,
-                  trueChild: _buildAccountIcon(context, loggedIn: true),
-                  falseChild: _buildAccountIcon(context, loggedIn: false),
-                ),
-                orElse: () => _buildAccountIcon(context, loggedIn: false),
+              PopupMenuButton<MenuAction>(
+                itemBuilder: (_) => <PopupMenuItem<MenuAction>>[
+                  const PopupMenuItem<MenuAction>(
+                    value: MenuAction.favorites,
+                    child: Text('Favorites'),
+                  ),
+                  const PopupMenuItem<MenuAction>(
+                    value: MenuAction.submit,
+                    child: Text('Submit'),
+                  ),
+                  const PopupMenuItem<MenuAction>(
+                    value: MenuAction.account,
+                    child: Text('Account'),
+                  ),
+                ],
+                onSelected: (MenuAction menuAction) async {
+                  switch (menuAction) {
+                    case MenuAction.favorites:
+                      return _favoritesSelected(context);
+                    case MenuAction.submit:
+                      return _submitSelected(context);
+                    case MenuAction.account:
+                      return _accountSelected(context);
+                  }
+                },
+                icon: const Icon(FluentIcons.more_vertical_24_filled),
               ),
             ],
             forceElevated: innerBoxIsScrolled,
@@ -111,97 +121,72 @@ class StoriesPage extends HookWidget {
     );
   }
 
-  Widget _buildSearchIcon(BuildContext context) {
-    return IconButton(
-      icon: const Icon(FluentIcons.search_24_filled),
-      tooltip: 'Search',
-      onPressed: () => Navigator.of(context).push<void>(
-        PageRouteBuilder<void>(
-          pageBuilder: (_, __, ___) => const StoriesSearchPage(),
-          transitionsBuilder:
-              (_, Animation<double> animation, __, Widget child) =>
-                  FadeTransition(opacity: animation, child: child),
-        ),
+  Future<void> _searchSelected(BuildContext context) {
+    return Navigator.of(context).push<void>(
+      PageRouteBuilder<void>(
+        pageBuilder: (_, __, ___) => const StoriesSearchPage(),
+        transitionsBuilder:
+            (_, Animation<double> animation, __, Widget child) =>
+                FadeTransition(opacity: animation, child: child),
       ),
     );
   }
 
-  Widget _buildSubmitIcon(BuildContext context) {
-    return IconButton(
-      icon: const Icon(FluentIcons.add_24_filled),
-      tooltip: 'Submit',
-      onPressed: () async {
-        if (await context.read(authRepositoryProvider).loggedIn) {
-          final bool success = await Navigator.of(context).push<bool>(
-            MaterialPageRoute<bool>(
-              builder: (_) => const SubmitPage(),
-              fullscreenDialog: true,
-            ),
-          );
-
-          if (success != null && success) {
-            await context.refresh(storyIdsProvider(StoryType.newStories));
-            context.read(storyTypeStateProvider).state = StoryType.newStories;
-            ScaffoldMessenger.of(context).showSnackBarQuickly(
-              SnackBar(
-                content: const Text(
-                  'Processing may take a moment, '
-                  'consider refreshing for an updated view',
-                ),
-                action: SnackBarAction(
-                  label: 'Refresh',
-                  onPressed: () =>
-                      context.refresh(storyIdsProvider(StoryType.newStories)),
-                ),
-              ),
-            );
-          }
-        } else {
-          ScaffoldMessenger.of(context).showSnackBarQuickly(
-            SnackBar(
-              content: const Text('Log in to submit'),
-              action: SnackBarAction(
-                label: 'Log in',
-                onPressed: () => Navigator.of(context).push<void>(
-                  MaterialPageRoute<void>(
-                    builder: (_) => const AccountPage(),
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-      },
-    );
-  }
-
-  Widget _buildFavoritesIcon(BuildContext context) {
-    return Center(
-      child: IconButton(
-        icon: const Icon(FluentIcons.star_line_horizontal_3_24_filled),
-        tooltip: 'Favorites',
-        onPressed: () => Navigator.of(context).push<void>(
-          MaterialPageRoute<void>(
-            builder: (_) => const FavoritesPage(),
-          ),
-        ),
+  Future<void> _favoritesSelected(BuildContext context) {
+    return Navigator.of(context).push<void>(
+      MaterialPageRoute<void>(
+        builder: (_) => const FavoritesPage(),
       ),
     );
   }
 
-  Widget _buildAccountIcon(BuildContext context, {@required bool loggedIn}) {
-    return Center(
-      child: IconButton(
-        key: ValueKey<bool>(loggedIn),
-        icon: Icon(loggedIn
-            ? FluentIcons.person_available_24_filled
-            : FluentIcons.person_24_filled),
-        tooltip: 'Account',
-        onPressed: () => Navigator.of(context).push<void>(
-          MaterialPageRoute<void>(
-            builder: (_) => const AccountPage(),
+  Future<void> _submitSelected(BuildContext context) async {
+    if (await context.read(authRepositoryProvider).loggedIn) {
+      final bool success = await Navigator.of(context).push<bool>(
+        MaterialPageRoute<bool>(
+          builder: (_) => const SubmitPage(),
+          fullscreenDialog: true,
+        ),
+      );
+
+      if (success != null && success) {
+        await context.refresh(storyIdsProvider(StoryType.newStories));
+        context.read(storyTypeStateProvider).state = StoryType.newStories;
+        ScaffoldMessenger.of(context).showSnackBarQuickly(
+          SnackBar(
+            content: const Text(
+              'Processing may take a moment, '
+              'consider refreshing for an updated view',
+            ),
+            action: SnackBarAction(
+              label: 'Refresh',
+              onPressed: () =>
+                  context.refresh(storyIdsProvider(StoryType.newStories)),
+            ),
+          ),
+        );
+      }
+    } else {
+      ScaffoldMessenger.of(context).showSnackBarQuickly(
+        SnackBar(
+          content: const Text('Log in to submit'),
+          action: SnackBarAction(
+            label: 'Log in',
+            onPressed: () => Navigator.of(context).push<void>(
+              MaterialPageRoute<void>(
+                builder: (_) => const AccountPage(),
+              ),
+            ),
           ),
         ),
+      );
+    }
+  }
+
+  Future<void> _accountSelected(BuildContext context) {
+    return Navigator.of(context).push<void>(
+      MaterialPageRoute<void>(
+        builder: (_) => const AccountPage(),
       ),
     );
   }
