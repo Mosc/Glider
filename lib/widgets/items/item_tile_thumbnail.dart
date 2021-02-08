91f53a4099cce94b0863ba21ed61f47f65f53d15
Mosc <daniel.moscoviter@gmail.com>
2021-02-08 21:26:56 +0100

    Open link on thumbnail click

--- lib/widgets/items/item_tile_thumbnail.dart
+++ lib/widgets/items/item_tile_thumbnail.dart
@@ -3,6 +3,7 @@ import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:glider/models/item.dart';
+import 'package:glider/utils/url_util.dart';
 import 'package:glider/widgets/common/tile_loading_block.dart';
 import 'package:glider/widgets/items/item_tile_header.dart';
 import 'package:octo_image/octo_image.dart';
@@ -18,23 +19,26 @@ class ItemTileThumbnail extends HookWidget {
   Widget build(BuildContext context) {
     final double size = ItemTileHeader.calculateHeight(context);
 
-    return Hero(
-      tag: 'item_thumbnail_${item.id}',
-      child: item.localOnly
-          ? _placeholderBuilder(context, size: size)
-          : OctoImage(
-              image: CachedNetworkImageProvider(item.thumbnailUrl),
-              imageBuilder: (_, Widget child) => ClipRRect(
-                borderRadius: _borderRadius,
-                child: child,
+    return GestureDetector(
+      onTap: () => UrlUtil.tryLaunch(item.url),
+      child: Hero(
+        tag: 'item_thumbnail_${item.id}',
+        child: item.localOnly
+            ? _placeholderBuilder(context, size: size)
+            : OctoImage(
+                image: CachedNetworkImageProvider(item.thumbnailUrl),
+                imageBuilder: (_, Widget child) => ClipRRect(
+                  borderRadius: _borderRadius,
+                  child: child,
+                ),
+                placeholderBuilder: (BuildContext context) =>
+                    _placeholderBuilder(context, size: size),
+                errorBuilder:
+                    OctoError.icon(icon: FluentIcons.error_circle_24_regular),
+                width: size,
+                height: size,
               ),
-              placeholderBuilder: (BuildContext context) =>
-                  _placeholderBuilder(context, size: size),
-              errorBuilder:
-                  OctoError.icon(icon: FluentIcons.error_circle_24_regular),
-              width: size,
-              height: size,
-            ),
+      ),
     );
   }
 
