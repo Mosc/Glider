b59442dcd3db429011624048b80ea1a88c448dbb
Mosc <daniel.moscoviter@gmail.com>
2021-11-09 19:40:08 +0100

    Add user report action because having this option seems incredibly important to the Google Play team

--- lib/models/user_menu_action.dart
+++ lib/models/user_menu_action.dart
@@ -3,10 +3,12 @@ import 'package:flutter/widgets.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:glider/commands/command.dart';
+import 'package:glider/commands/report_command.dart';
 import 'package:glider/commands/user_options_command.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 enum UserMenuAction {
+  report,
   copy,
   share,
 }
@@ -14,6 +16,8 @@ enum UserMenuAction {
 extension UserMenuActionExtension on UserMenuAction {
   String title(BuildContext context) {
     switch (this) {
+      case UserMenuAction.report:
+        return AppLocalizations.of(context)!.report;
       case UserMenuAction.copy:
         return AppLocalizations.of(context)!.copy;
       case UserMenuAction.share:
@@ -23,6 +27,8 @@ extension UserMenuActionExtension on UserMenuAction {
 
   IconData get icon {
     switch (this) {
+      case UserMenuAction.report:
+        return FluentIcons.mail_error_24_regular;
       case UserMenuAction.copy:
         return FluentIcons.copy_24_regular;
       case UserMenuAction.share:
@@ -33,6 +39,8 @@ extension UserMenuActionExtension on UserMenuAction {
   CommandMixin command(BuildContext context, WidgetRef ref,
       {required String id}) {
     switch (this) {
+      case UserMenuAction.report:
+        return ReportCommand(context, ref, id: id);
       case UserMenuAction.copy:
         return UserOptionsCommand.copy(context, ref, id: id);
       case UserMenuAction.share:
