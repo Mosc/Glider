713b59f5e337ad8bdfec72dc6187da89e4a9abbc
Mosc <daniel.moscoviter@gmail.com>
2022-01-27 22:22:07 +0100

    Only try setting high refresh rate on Android

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -17,7 +19,9 @@ class App extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    useMemoized(FlutterDisplayMode.setHighRefreshRate);
+    if (Platform.isAndroid) {
+      useMemoized(FlutterDisplayMode.setHighRefreshRate);
+    }
 
     useMemoized(
       () => SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge),
