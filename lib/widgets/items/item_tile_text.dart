527ecc4656f42b4fc1cbe404f0b397643c034ee1
Mosc <daniel.moscoviter@gmail.com>
2022-04-02 16:24:13 +0200

    Animate item text changes

--- lib/widgets/items/item_tile_text.dart
+++ lib/widgets/items/item_tile_text.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:glider/models/item.dart';
 import 'package:glider/widgets/common/decorated_html.dart';
 import 'package:glider/widgets/common/fade_hero.dart';
+import 'package:glider/widgets/common/smooth_animated_size.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class ItemTileText extends HookConsumerWidget {
@@ -16,7 +17,9 @@ class ItemTileText extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return FadeHero(
       tag: 'item_${item.id}_text',
-      child: DecoratedHtml(item.text!),
+      child: SmoothAnimatedSize(
+        child: DecoratedHtml(item.text!),
+      ),
     );
   }
 }
