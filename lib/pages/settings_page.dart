d6011da32b1848cb63c784822619b534adfe24df
Mosc <daniel.moscoviter@gmail.com>
2022-07-14 20:38:39 +0200

    Use HookConsumerWidget for all widgets

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 // ignore: depend_on_referenced_packages
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:glider/widgets/common/floating_app_bar_scroll_view.dart';
 import 'package:glider/widgets/settings/settings_body.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SettingsPage extends HookWidget {
+class SettingsPage extends HookConsumerWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       body: FloatingAppBarScrollView(
         title: Text(AppLocalizations.of(context).settings),
