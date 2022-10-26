44174fca5ac81dd7c79d7344c6c7b2385a76df63
ynotzort <ynotzort@users.noreply.github.com>
2022-10-26 20:45:02 +0200

    Update lib/models/item.dart
    Co-authored-by: Mosc <daniel.moscoviter@gmail.com>

--- lib/models/item.dart
+++ lib/models/item.dart
@@ -91,7 +91,7 @@ class Item with _$Item {
       active && type != ItemType.job && type != ItemType.pollopt;
 
   late final bool visitable =
-      active && type == ItemType.story;
+      active && type != ItemType.job && type != ItemType.pollopt;
 
   bool get editable {
     final num? timeAgoHours = _timeAgoHours;
