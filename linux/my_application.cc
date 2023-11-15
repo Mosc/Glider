d68f59222132a64f84f65ac36a1bb7242174907f
Fabian Freund <mail@fabi.online>
2023-11-15 17:03:50 +0100

    Update linux/my_application.cc
    Co-authored-by: Mosc <daniel.moscoviter@gmail.com>

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -44,7 +44,7 @@ static void my_application_activate(GApplication* application) {
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "glider");
+    gtk_window_set_title(window, "Glider");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
