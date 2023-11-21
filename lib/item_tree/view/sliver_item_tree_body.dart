78e959cd8bf58cfb20df037510d6a4638b087445
Mosc <daniel.moscoviter@gmail.com>
2023-11-21 12:04:39 +0100

    Simplify item tree

--- lib/item_tree/view/sliver_item_tree_body.dart
+++ lib/item_tree/view/sliver_item_tree_body.dart
@@ -1,12 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:glider/app/container/app_container.dart';
 import 'package:glider/auth/cubit/auth_cubit.dart';
 import 'package:glider/common/constants/app_animation.dart';
 import 'package:glider/common/mixins/data_mixin.dart';
-import 'package:glider/item/cubit/item_cubit.dart';
 import 'package:glider/item/widgets/indented_widget.dart';
 import 'package:glider/item/widgets/item_loading_tile.dart';
 import 'package:glider/item/widgets/item_tile.dart';
@@ -15,7 +12,7 @@ import 'package:glider/l10n/extensions/app_localizations_extension.dart';
 import 'package:glider/settings/cubit/settings_cubit.dart';
 import 'package:glider_domain/glider_domain.dart';
 
-class SliverItemTreeBody extends StatefulWidget {
+class SliverItemTreeBody extends StatelessWidget {
   const SliverItemTreeBody(
     this._itemTreeCubit,
     this._itemCubitFactory,
@@ -34,43 +31,12 @@ class SliverItemTreeBody extends StatefulWidget {
   final String? storyUsername;
 
   @override
-  State<SliverItemTreeBody> createState() => _SliverItemTreeBodyState();
-}
-
-class _SliverItemTreeBodyState extends State<SliverItemTreeBody> {
-  final Map<int, ItemCubit> _itemCubits = {};
-
-  @override
-  void initState() {
-    _updateItemCubits(widget._itemTreeCubit.state);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    for (final itemCubit in _itemCubits.values) {
-      unawaited(itemCubit.close());
-    }
-
-    super.dispose();
-  }
-
-  void _updateItemCubits(ItemTreeState state) {
-    if (state.data case final descendants?) {
-      for (final descendant in descendants) {
-        _itemCubits[descendant.id] ??= widget._itemCubitFactory(descendant.id);
-      }
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return BlocConsumer<ItemTreeCubit, ItemTreeState>(
-      bloc: widget._itemTreeCubit,
-      listenWhen: (previous, current) => previous.data != current.data,
+      bloc: _itemTreeCubit,
+      listenWhen: (previous, current) =>
+          previous.newDescendantsCount != current.newDescendantsCount,
       listener: (context, state) {
-        _updateItemCubits(state);
-
         if (state.newDescendantsCount > 0) {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(
@@ -81,7 +47,7 @@ class _SliverItemTreeBodyState extends State<SliverItemTreeBody> {
         }
       },
       builder: (context, state) => BlocBuilder<SettingsCubit, SettingsState>(
-        bloc: widget._settingsCubit,
+        bloc: _settingsCubit,
         buildWhen: (previous, current) =>
             previous.useActionButtons != current.useActionButtons,
         builder: (context, settingsState) => state.whenOrDefaultSlivers(
@@ -90,7 +56,7 @@ class _SliverItemTreeBodyState extends State<SliverItemTreeBody> {
               depth: 1,
               child: ItemLoadingTile(type: ItemType.comment),
             ),
-            itemCount: widget.childCount,
+            itemCount: childCount,
           ),
           nonEmpty: () => SliverList.builder(
             itemCount: state.viewableData!.length,
@@ -100,7 +66,7 @@ class _SliverItemTreeBodyState extends State<SliverItemTreeBody> {
               settingsState,
             ),
           ),
-          onRetry: () async => widget._itemTreeCubit.load(),
+          onRetry: () async => _itemTreeCubit.load(),
         ),
       ),
     );
@@ -113,12 +79,13 @@ class _SliverItemTreeBodyState extends State<SliverItemTreeBody> {
   ) {
     return IndentedWidget(
       depth: descendant.isPart ? 0 : descendant.depth,
-      child: ItemTile(
-        _itemCubits[descendant.id]!,
-        widget._authCubit,
-        widget._settingsCubit,
+      child: ItemTile.create(
+        _itemCubitFactory,
+        _authCubit,
+        _settingsCubit,
         key: ValueKey(descendant.id),
-        storyUsername: widget.storyUsername,
+        id: descendant.id,
+        storyUsername: storyUsername,
         loadingType: ItemType.comment,
         collapsedCount: state.collapsedIds.contains(descendant.id)
             ? state.getDescendants(descendant)?.length
@@ -129,7 +96,7 @@ class _SliverItemTreeBodyState extends State<SliverItemTreeBody> {
                 true),
         onTap: (context, item) async {
           if (!item.isDeleted) {
-            widget._itemTreeCubit.toggleCollapsed(item.id);
+            _itemTreeCubit.toggleCollapsed(item.id);
           }
 
           await Scrollable.ensureVisible(
