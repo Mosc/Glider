a5208a008729e282f3af022e83f4f2916dda2b0c
Mosc <daniel.moscoviter@gmail.com>
2023-11-24 13:04:29 +0100

    Do prefer external non-browser apps over custom tabs

--- lib/common/extensions/uri_extension.dart
+++ lib/common/extensions/uri_extension.dart
@@ -3,20 +3,20 @@ import 'package:url_launcher/url_launcher.dart';
 extension UriExtension on Uri {
   Future<bool> tryLaunch({String? title, required bool useInAppBrowser}) async {
     if (await canLaunchUrl(this)) {
-      if (useInAppBrowser &&
-          await supportsLaunchMode(LaunchMode.inAppBrowserView)) {
+      if (await supportsLaunchMode(LaunchMode.externalNonBrowserApplication)) {
         final success = await launchUrl(
           this,
-          mode: LaunchMode.inAppBrowserView,
+          mode: LaunchMode.externalNonBrowserApplication,
           webOnlyWindowName: title,
         );
         if (success) return true;
       }
 
-      if (await supportsLaunchMode(LaunchMode.externalNonBrowserApplication)) {
+      if (useInAppBrowser &&
+          await supportsLaunchMode(LaunchMode.inAppBrowserView)) {
         final success = await launchUrl(
           this,
-          mode: LaunchMode.externalNonBrowserApplication,
+          mode: LaunchMode.inAppBrowserView,
           webOnlyWindowName: title,
         );
         if (success) return true;
